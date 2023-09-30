97017e7b44670e1e042b3a6946ccb55818f9729d
Vsevolod <shpakyn.26@gmail.com>
2023-10-01 00:55:40 +0300

    token

---
+++ src/token.rs
@@ -0,0 +1,48 @@
+use std::fmt;
+use std::fmt::{Formatter, write};
+
+#[derive(Debug, PartialEq, Clone)]
+enum Token {
+    Illegal,
+    EOF,
+
+    //Identifiers + literals
+    Ident(String),
+    Integer(i32),
+
+    //Operators
+    Assign, // =
+    Plus, // +
+
+    //Delimiters
+    Comma, // ,
+    Semicolon, // ;
+    LParen, // (
+    RParen, // )
+    LBrace, // {
+    RBrace, // }
+
+    //Keywords
+    Function,
+    Let,
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Token::Assign => write!(f, "="),
+            Token::Ident(id) => write!(f, "{}", id),
+            Token::Integer(i32) => write!(f, "{}", i32),
+            Token::Plus => write!(f, "{}", "+"),
+            Token::Comma => write!(f, "{}", ","),
+            Token::Semicolon => write!(f, "{}", ";"),
+            Token::LParen => write!(f, "{}", "("),
+            Token::RParen => write!(f, "{}", ")"),
+            Token::LBrace => write!(f, "{}", "{"),
+            Token::RBrace => write!(f, "{}", "}"),
+            Token::Function => write!(f, "{}", "fn"),
+            Token::Let => write!(f, "{}", "let"),
+            token => write!(f, "{:?}", token),
+        }
+    }
+}
